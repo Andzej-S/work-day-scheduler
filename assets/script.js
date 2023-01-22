03dcb3cd5e7992fa378f3a38b5517e75586c8614
Andzej-S <andrewstreckul@gmail.com>
2023-01-22 23:05:19 +0000

    add function that changes color inside time-block class based on current time

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,25 @@
 const currentDate = moment().format('dddd, MMMM Do');
+const currentTime = moment();
+const timeBlocks = $('.time-block');
+
+// Display current day
 $('#currentDay').text(currentDate);
 
+// Function that color-codes time-block based on past, present and future when the timeblock is viewed
+timeBlocks.each(function() {
+    const timeBlockTime = moment($(this).data('time'), 'HH:mm');
+    if (currentTime.isBefore(timeBlockTime)) {
+      $(this).addClass('future');
+    } else if (currentTime.isAfter(timeBlockTime)) {
+      $(this).addClass('past');
+    } else {
+      $(this).addClass('present');
+    }
+  });
 
-// * Display the current day at the top of the calender when a user opens the planner.
+  
  
-// * Present timeblocks for standard business hours when the user scrolls down.
- 
-// * Color-code each timeblock based on past, present, and future when the timeblock is viewed.
- 
-// * Allow a user to enter an event when they click a timeblock
 
 // * Save the event in local storage when the save button is clicked in that timeblock.
 
 // * Persist events between refreshes of a page
-
-// * The following animation demonstrates the application functionality:

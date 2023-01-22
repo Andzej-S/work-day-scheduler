2c10957304767b3508ec645da8b29d4bdc4a540d
Andzej-S <andrewstreckul@gmail.com>
2023-01-22 23:44:49 +0000

    add event listener to save button, and add id to html hours to separate them

--- assets/script.js
+++ assets/script.js
@@ -15,11 +15,22 @@ timeBlocks.each(function() {
     } else {
       $(this).addClass('present');
     }
-  });
+});
 
-  
- 
+// Event listener for saveBtn click
+$('.saveBtn').on('click', function () {
+    let textContent = $(this).siblings('.description').val();
+    let hour = $(this).parent().attr('id');
+    localStorage.setItem(hour, textContent);
+});
 
-// * Save the event in local storage when the save button is clicked in that timeblock.
-
-// * Persist events between refreshes of a page
+// Get items from local storage
+$("#hour9 .description").val(localStorage.getItem("hour9"));
+$("#hour10 .description").val(localStorage.getItem("hour10"));
+$("#time11 .description").val(localStorage.getItem("hour11"));
+$("#hour12 .description").val(localStorage.getItem("hour12"));
+$("#hour13 .description").val(localStorage.getItem("hour13"));
+$("#hour14 .description").val(localStorage.getItem("hour14"));
+$("#hour15 .description").val(localStorage.getItem("hour15"));
+$("#hour16 .description").val(localStorage.getItem("hour16"));
+$("#hour17 .description").val(localStorage.getItem("hour17"));
